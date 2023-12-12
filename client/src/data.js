2015aef5a75e19df8c7695cb7a17ff9180a8eb57
Cormac <cormac_gudge@hotmail.com>
2023-12-12 16:27:25 +0000

    added dummy data injector

---
+++ client/src/data.js
@@ -0,0 +1,44 @@
+export const STATES = [
+  "Dead",
+  "Alive",
+  "Unkown",
+]
+
+export const ROLES = [
+  "Unkown",
+  "Hunter",
+  "Biologist",
+  "Bodyguard",
+  "Cook",
+  "Doctor",
+  "Engineer",
+  "Janitor",
+  "Medic",
+  "Mortician",
+  "Quartermaster",
+  "Recruit/Trainee",
+  "Researcher",
+  "Supervisor",
+  "Tracker",
+  "Maniac",
+  "Outsider",
+  "Prisoner",
+  "Banshee",
+  "Bogeyman",
+  "Necromancer",
+  "Serial Killer",
+  "Vampire",
+  "Werewolf",
+]
+
+export const CHARS = [
+  "Unkown",
+  "Envious",
+  "Gluttonous",
+  "Drunkard",
+]
+
+export const STATUSES = [
+  "Normal",
+  "Malfunctioning",
+]
