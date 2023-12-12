8683514d5eccbb6a8e6d7a2ef029dcd91cc1497b
Cormac <cormac_gudge@hotmail.com>
2023-12-12 16:34:26 +0000

    added storyteller version of playerindicator

--- client/src/components/PlayerIndicator.jsx
+++ client/src/components/PlayerIndicator.jsx
@@ -1,6 +1,33 @@
+import {useContext} from "react";
 import {Box, Button, Typography} from '@mui/material';
+import {UserContext} from "../App.jsx";
+import {STATES, ROLES, CHARS, STATUSES} from "../data.js";
 
-function PlayerIndicator({id, name, label, handleClick, vertical}) {
+function PlayerIndicator(props) {
+
+  const user = useContext(UserContext);
+
+  const getUserTypeCheckedComponent = () => {
+
+    if (user.type === 0) {
+      return <StorytellerPlayerIndicator {...props}/>
+    } else if (user.type === 1) {
+      return <RegularPlayerIndicator {...props} />
+    } else {
+      return false
+    }
+
+  }
+
+  return getUserTypeCheckedComponent();
+
+}
+
+export default PlayerIndicator
+
+
+
+function RegularPlayerIndicator({id, name, label, handleClick, vertical}) {
 
   return (
     <Box sx={{
@@ -39,6 +66,57 @@ function PlayerIndicator({id, name, label, handleClick, vertical}) {
       </Button>
     </Box>
   );
+
 }
 
-export default PlayerIndicator
+
+
+function StorytellerPlayerIndicator({
+  id, name, state, role, char, status, 
+  rState, rRole, rChar, rStatus, handleClick, vertical}) {
+
+  return (
+    <Box sx={{
+      width: vertical ? "100%" : "20%",
+      aspectRatio: "1/1",
+      flexDirection: "column",
+      justifyContent: "flex-start",
+      display: "flex",
+      overflow: "clip", 
+    }}>
+      <Button variant="contained" onClick={() => {handleClick(id)}} sx={{
+        flexGrow: 1, 
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        m: 0.8,
+        overflow: "inherit" 
+      }}>
+        <Typography>{name}</Typography>
+        <Box sx={{
+          display: "flex", 
+          justifyContent: "center", 
+          flexDirection: "column",
+          flexGrow: 1,
+          overflow: "inherit" 
+        }}>
+          <Typography 
+            variant="subtitle" 
+            sx={{
+              wordBreak: "break-word",
+              overflow: "inherit",
+              textTransform: "none",
+            }}>
+              {STATES[state]}{state !== rState ? "*" : ""}
+              <br />
+              {ROLES[role]}{role !== rRole ? "*" : ""}
+              <br />
+              {CHARS[char]}{char !== rChar ? "*" : ""}
+              <br />
+              {STATUSES[status]}{status !== rStatus ? "*" : ""}
+          </Typography>
+        </Box>
+      </Button>
+    </Box>
+  );
+
+}
