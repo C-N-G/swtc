b02ce7f4bfab324980587e828fc232fb315d64ae
Cormac Gudge <cormac_gudge@hotmail.com>
2023-12-24 06:19:55 +0000

    added storyteller character window

--- client/src/components/Character.jsx
+++ client/src/components/Character.jsx
@@ -1,10 +1,23 @@
 import { useContext } from 'react'
 import {Card, Typography, Grid, Paper}from '@mui/material';
 import {UserContext} from "../App.jsx";
+import {STATES, ROLES, CHARS, STATUSES} from "../data.js";
 
-function Character() {
+function Character(props) {
 
   const user = useContext(UserContext);
+
+  const getUserTypeCheckedComponent = () => {
+
+    if (user?.type === 0) {
+      return <StoryTellerCharacter {...props} />
+    } else if (user?.type === 1) {
+      return <PlayerCharacter user={user} {...props} />
+    } else {
+      return false
+    }
+
+  }
 
   return (
     <Card sx={{
@@ -17,31 +30,49 @@ function Character() {
       boxSizing: "border-box",
       p: 2,
     }}>
-      <Grid container justifyContent="left" mb={2} spacing={2}>
-        <Grid item xs={6}>
-          <Paper elevation={2} sx={{backgroundColor: "lightgreen"}}>
-            <Typography variant="h6">State: {user.state === 1 ? "Alive" : "Dead"}</Typography> 
-          </Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper elevation={2} sx={{backgroundColor: "lightgreen"}}>
-            <Typography variant="h6">Status: {user.status}</Typography> 
-          </Paper>
-        </Grid>
-        <Grid item xs textAlign="right">
-          <Typography>Role</Typography> 
-          <Typography>Characteristic</Typography> 
-          <Typography>Team</Typography> 
-        </Grid>
-        <Grid item xs textAlign="left">
-          <Typography fontWeight={600}>{user.role}</Typography> 
-          <Typography fontWeight={600}>{user.char}</Typography> 
-          <Typography fontWeight={600}>{user.team === 0 ? "Loyalist" : "Subversive"}</Typography> 
-        </Grid>
-      </Grid>
-      <iframe src="https://drive.google.com/file/d/1BSgDm_VNXi-e2_0v5L5Xd781-kFyde7k/preview" style={{flexGrow: 1}} allow="autoplay"></iframe>
+      {getUserTypeCheckedComponent()}
     </Card>
   );
 }
 
 export default Character
+
+
+function PlayerCharacter({user}) {
+
+  return (<>
+      <Grid container justifyContent="left" mb={2} spacing={2}>
+      <Grid item xs={6}>
+        <Paper elevation={2} sx={{backgroundColor: "lightgreen"}}>
+          <Typography variant="h6">State: {STATES[user.rState]}</Typography> 
+        </Paper>
+      </Grid>
+      <Grid item xs={6}>
+        <Paper elevation={2} sx={{backgroundColor: "lightgreen"}}>
+          <Typography variant="h6">Status: {STATUSES[user.rStatus]}</Typography> 
+        </Paper>
+      </Grid>
+      <Grid item xs textAlign="right">
+        <Typography>Role</Typography> 
+        <Typography>Characteristic</Typography> 
+        <Typography>Team</Typography> 
+      </Grid>
+      <Grid item xs textAlign="left">
+        <Typography fontWeight={600}>{ROLES[user.rRole]}</Typography> 
+        <Typography fontWeight={600}>{CHARS[user.rChar]}</Typography> 
+        <Typography fontWeight={600}>{user.team === 0 ? "Loyalist" : "Subversive"}</Typography> 
+      </Grid>
+    </Grid>
+    <iframe src="https://drive.google.com/file/d/1BSgDm_VNXi-e2_0v5L5Xd781-kFyde7k/preview" style={{flexGrow: 1}} allow="autoplay"></iframe>
+  </>)
+
+}
+
+
+function StoryTellerCharacter({session}) {
+
+  return (<>
+    <Typography variant="h6">Session Id: {session}</Typography>
+  </>)
+
+}
