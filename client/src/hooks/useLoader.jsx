4fc02594bec014db6f6be181c80a78570944f49c
Cormac <cormac_gudge@hotmail.com>
2024-01-20 14:31:17 +0000

    updated loader function

--- client/src/hooks/useLoader.jsx
+++ client/src/hooks/useLoader.jsx
@@ -1,92 +1,92 @@
-import default_roles from "../strings/roles.json";
-import default_chars from "../strings/chars.json";
-import default_states from "../strings/states.json";
-import default_statuses from "../strings/statuses.json";
+const modules = import.meta.glob(["../strings/*.json", "../strings/modules/*/*.json"], {eager: true})
 
-import module_sv_roles from "../strings/modules/1_Standard Procedure/roles.json";
-import module_sv_chars from "../strings/modules/1_Standard Procedure/chars.json";
-
-import module_p_roles from "../strings/modules/2_Placeholder Module/roles.json";
-import module_p_chars from "../strings/modules/2_Placeholder Module/chars.json";
-
-// const modules = import.meta.glob(["../strings/*.json", "../strings/modules/*/*.json"], {eager: true})
-
-// const modules = import.meta.glob("../strings/modules/*/*.json", {eager: true})
+// this might be a computationally expensive function
 
 function useLoader() {
 
-  // hard coded import methods
+  function import_json(file_path, load_origin, load_target) {
 
-  const GameData = {
-    roles: [
-      ...default_roles, 
-      ...module_sv_roles,
-      ...module_p_roles,
-    ],
-    chars: [
-      ...default_chars, 
-      ...module_sv_chars,
-      ...module_p_chars,
-    ],
-    states: [...default_states],
-    statuses: [...default_statuses],
-    modules: {
-      "Standard Procedure": {
-        roles: module_sv_roles.map(role => role.Name),
-        chars: module_sv_chars.map(role => role.Name),
-      },
-      "Placeholder Module": {
-        roles: module_p_roles.map(role => role.Name),
-        chars: module_p_chars.map(role => role.Name),
+    const property = file_path.split("/").pop().slice(0, -5);
+
+    if (file_path.includes("modules")) {
+
+      const module_name = file_path.split("/")[3];
+      const property_names = load_origin[file_path].default.map((ele) => ele.Name);
+
+      if (!Object.hasOwn(load_target.modules, module_name)) {
+        load_target.modules[module_name] = {};
       }
+
+      load_target.modules[module_name][property] = property_names;
+
     }
+
+    load_target[property] = load_target[property].concat(load_origin[file_path].default);
+
   }
 
+  function convert_modules_to_array(load_target) {
 
-  // dynamic import method
+    load_target.modules = Object.keys(load_target.modules).map((mod) => {
+      return {
+        Name: mod,
+        chars: load_target.modules[mod].chars,
+        roles: load_target.modules[mod].roles
+      }
+    })
 
-  // function import_json(file_path, load_origin, load_target) {
+  }
 
-  //   let properties = ["roles", "chars", "states", "statuses"];
+  function sort_game_data(load_target) {
 
-  //   properties.forEach((property) => {
-  //     if (file_path.endsWith(property + ".json")) {
-  //       load_target[property] = load_target[property].concat(load_origin[file_path].default);
+    function sort_name(a, b) {
+      const nameA = a.Name.toUpperCase();
+      const nameB = b.Name.toUpperCase();
+      if (nameA < nameB) return -1;
+      if (nameA > nameB) return 1;
+      return 0;
+    }
 
-  //       const module_name = file_path.split("/")[3].split("_")[1];
-  //       const property_names = load_origin[file_path].default.map((ele) => ele.Name);
+    function sort_module(a, b) {
+      const modA = Number(a.Name.split("_")[0]);
+      const modB = Number(b.Name.split("_")[0]);
+      if (modA < modB) return -1;
+      if (modA > modB) return 1;
+      return 0;
+    }
 
-  //       // this is a problem in that states and statuses are being added to the game object even though they are empty
-  //       // need to have some kind of sorting system
-  //       if (!Object.hasOwn(load_target.modules, module_name)) {
-  //         load_target.modules[module_name] = {};
-  //       }
-  //       load_target.modules[module_name][property] = property_names;
-  //     }
-  //   })
+    load_target.chars.sort(sort_name);
+    load_target.roles.sort(sort_name);
+    load_target.modules.sort(sort_module);
 
-  // }
+  }
 
-  // const GameData = {
-  // roles: [],
-  // chars: [],
-  // states: [],
-  // statuses: [],
-  // modules: {}
-  // }
+  function remove_module_prefix(load_target) {
 
-  // Object.keys(modules).forEach((path) => {
+    load_target.modules = load_target.modules.map((mod) => {
+      return {
+        ...mod,
+        Name: mod.Name.split("_")[1]
+      }
+    })
 
-  //   import_json(path, modules, GameData);
+  }
 
+  const GameData = {
+  chars: [],
+  roles: [],
+  states: [],
+  statuses: [],
+  modules: {}
+  }
 
-  //   // modules[path]
-  // })
+  Object.keys(modules).forEach((path) => import_json(path, modules, GameData))
 
+  convert_modules_to_array(GameData);
 
+  sort_game_data(GameData);
 
-  // console.log(modules["../strings/chars.json"].default)
-  
+  remove_module_prefix(GameData);
 
   console.log(GameData);
 
