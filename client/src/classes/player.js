42f9dd7d160f530b8aaa2c7226ebca0b5add1ba0
Cormac <cormac_gudge@hotmail.com>
2024-02-02 21:21:42 +0000

    added rTeam to player class

--- client/src/classes/player.js
+++ client/src/classes/player.js
@@ -3,49 +3,52 @@ export default class Player {
   #id;        // unique user identifier 
   #name;      // user name
   #type;      // type of user. 0 = narrator, 1 = player
-  #role;      // player role
-  #char;      // player characteristic
-  #status;    // player status
   #label;     // quick label of targetted player
   #notes;     // notes of targetted player
-  #team;      // player team. 0 = loyalist, 1 = subversives
+  #char;      // player characteristic
+  #role;      // player role
+  #status;    // player status
   #state;     // player state. 0 = dead, 1 = alive
-  #rRole;     // real player role shown to narrator
+  #team;      // player team
   #rChar;     // real player characteristic shown to narrator
+  #rRole;     // real player role shown to narrator
   #rStatus;   // real player status shown to narrator
   #rState;    // real player state shown to narrator
+  #rTeam;     // real player team shown to narrator
 
   constructor(
     id, 
     name, 
     type = 1, 
-    role = 0, 
-    char = 0, 
-    status = 0, 
     label = "", 
     notes = "", 
+    char = 0, 
+    role = 0, 
+    status = 0, 
     state = 1,
     team = 0,
-    rRole = 0,
     rChar = 0,
+    rRole = 0,
     rStatus = 0,
     rState = 1,
+    rTeam = 0
   ) {
 
     this.#id = id;
     this.#name = name;
     this.#type = type;
-    this.#role = role;
-    this.#char = char;
-    this.#status = status;
     this.#label = label;
     this.#notes = notes;
+    this.#char = char;
+    this.#role = role;
+    this.#status = status;
     this.#state = state;
     this.#team = team;
-    this.#rRole = rRole;
     this.#rChar = rChar;
+    this.#rRole = rRole;
     this.#rStatus = rStatus;
     this.#rState = rState;
+    this.#rTeam = rTeam;
 
   }
 
@@ -54,17 +57,18 @@ export default class Player {
       id: this.#id,
       name: this.#name,
       type: this.#type,
-      role: this.#role,
-      char: this.#char,
-      status: this.#status,
       label: this.#label,
       notes: this.#notes,
+      char: this.#char,
+      role: this.#role,
+      status: this.#status,
       state: this.#state,
       team: this.#team,
-      rRole: this.#rRole,
       rChar: this.#rChar,
+      rRole: this.#rRole,
       rStatus: this.#rStatus,
-      rState: this.#rState
+      rState: this.#rState,
+      rTeam: this.#rTeam
     }
   }
   
