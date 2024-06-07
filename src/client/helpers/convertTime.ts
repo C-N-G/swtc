edcde5acc9854d59d7193f6446a1c0ccd666f2b8
Cormac <cormac_gudge@hotmail.com>
2024-06-08 00:33:20 +0100

    convert time convert helper to ts

--- src/client/helpers/convertTime.ts
+++ src/client/helpers/convertTime.ts
@@ -1,18 +1,18 @@
-export default function convertTime(seconds) {
+export default function convertTime(seconds: number): string {
   
   if (seconds < 60) {
     return `${Math.floor(seconds)}s`;
   }
 
-  let minutes = Math.floor(seconds/60);
-  let returnSeconds = Math.floor(seconds - minutes*60);
+  let minutes: string | number = Math.floor(seconds/60);
+  let returnSeconds: string | number = Math.floor(seconds - minutes*60);
   if (returnSeconds < 10) returnSeconds = '0' + returnSeconds;
 
   if (minutes < 60) {
     return `${minutes}m:${returnSeconds}s`;
   }
 
-  let hours = Math.floor(seconds/60/60);
+  const hours = Math.floor(seconds/60/60);
   minutes -= hours*60;
   if (minutes < 10) minutes = '0' + minutes;
   return `${hours}h:${minutes}s:${returnSeconds}m`;
