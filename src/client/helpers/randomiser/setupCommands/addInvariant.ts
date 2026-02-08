6378d78e7a17d8031cad2d803543706fd27e0293
Cormac Gudge <cormac_gudge@hotmail.com>
2026-02-08 20:45:41 +0000

    Update addInvariant to support changing real attributes and team too

--- src/client/helpers/randomiser/setupCommands/addInvariant.ts
+++ src/client/helpers/randomiser/setupCommands/addInvariant.ts
@@ -1,9 +1,12 @@
+import Role from "../../../classes/role";
+import GameData from "../../../strings/_gameData";
 import { OperatingPlayer, Randomiser, SetupCommandParams } from "../randomiser";
 
 export default function addInvariant(aPlayer: OperatingPlayer, params: SetupCommandParams, randomiser: Randomiser) {
 
   const takenSetsTarget = `${params.targetName}s`;
   const playerAttribute = params.targetName;
+  const playerRealAttribute = `r${params.targetName.slice(0, 1).toUpperCase()}${params.targetName.slice(1)}`;
 
   if (takenSetsTarget !== "chars" && takenSetsTarget !== "roles") {
     throw new Error(`add target not set properly, found ${takenSetsTarget} for taken set`);
@@ -13,6 +16,10 @@ export default function addInvariant(aPlayer: OperatingPlayer, params: SetupComm
     throw new Error(`add target not set properly, found ${playerAttribute} for player attribute`);
   }
 
+  if (playerRealAttribute !== "rChar" && playerRealAttribute !== "rRole") {
+    throw new Error(`add target not set properly, found ${playerRealAttribute} for player attribute`);
+  }
+
   // delete initially randomised target from the taken targets
   randomiser.takenSets[takenSetsTarget].delete(aPlayer.playerObj[playerAttribute]);
 
@@ -20,6 +27,11 @@ export default function addInvariant(aPlayer: OperatingPlayer, params: SetupComm
   const targetIsAlreadyInUse = randomiser.takenSets[takenSetsTarget].has(targetId);
   if (!targetIsAlreadyInUse) {
     aPlayer.playerObj[playerAttribute] = targetId;
+    aPlayer.playerObj[playerRealAttribute] = targetId;
+    if (playerAttribute === "role") {
+      const team = (params.targetArray[targetId] as Role).team;
+      aPlayer.playerObj["team"] = GameData.teams.indexOf(team);
+    }
     randomiser.takenSets[takenSetsTarget].add(targetId);
   } 
 
